c1373576db6fe2c262ad9b7bc0309a727d56eec6
jpalomino10 <54417944+jpalomino10@users.noreply.github.com>
2020-10-01 22:09:00 -0500

    updated environments files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,17 +2,17 @@ export const environment = {
   production: true,
   endpoint: '',
   captcha: {
-    secret_key: '',
-    web_key: ''
+    secret_key: '6LcbvNIZAAAAAA1TF4_6bbnHu9Tc1rW5q9B-3I3U',
+    web_key: '6LcbvNIZAAAAAA2ei5N24OOOMHsdWVRm4tqRJ5ew'
   },
   firebaseConfig : {
-    apiKey: "AIzaSyBKCZiHez2Hm_91z7K0LXljPuqeWOVV4eY",
-    authDomain: "merakilab-e284b.firebaseapp.com",
-    databaseURL: "https://merakilab-e284b.firebaseio.com",
-    projectId: "merakilab-e284b",
-    storageBucket: "merakilab-e284b.appspot.com",
-    messagingSenderId: "647931356436",
-    appId: "1:647931356436:web:cfe7a2f8e4e6953b288e01",
-    measurementId: "G-KWYW1ZN9XZ"
+    apiKey: "AIzaSyBRaCSSEMOc3EGDC7UR7CiQTqRlpf0qn5U",
+    authDomain: "merakilab-prod.firebaseapp.com",
+    databaseURL: "https://merakilab-prod.firebaseio.com",
+    projectId: "merakilab-prod",
+    storageBucket: "merakilab-prod.appspot.com",
+    messagingSenderId: "603511129035",
+    appId: "1:603511129035:web:77c668c241062dc7dc2c4e",
+    measurementId: "G-SB74YM5BDX"
   }
 };
